f1721e0641fd667a71fee0468b183834ecf24595
Liwen M <liwen.ma@temple.edu>
2018-11-07 18:09:26 -0500

    hw finished

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('barbecue burger', 'false');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheeseburger', 'true');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo burger', 'false');
